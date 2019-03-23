7da0f800a8b37e0452f8d785828edaf6eb721a5d
Jacques <jacques@ronaldi.com>
2019-03-22 21:25:54 -0400

    Initial version of Contacts2Go. Currently only dumps contact name to screen.

---
+++ Contacts2Go/Contacts.fs
@@ -0,0 +1,29 @@
+﻿namespace Contacts2Go
+
+open Android.App
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Android.Graphics
+open Android.Provider
+open Android.Content
+
+module Contacts =
+
+    let getContactsInfo (cur:Android.Database.ICursor) : (string * string)[] =
+        [ while cur.MoveToNext() do yield (cur.GetString(0), cur.GetString(1)) ]
+        |> Array.ofList
+
+    let GetContactCursor (activity:Activity) =
+        let contacts =
+            let contactsUri = ContactsContract.Contacts.ContentUri
+            let contactProjection = [|
+                    ContactsContract.Contacts.InterfaceConsts.Id;
+                    ContactsContract.Contacts.InterfaceConsts.DisplayName
+                |]
+            let cursor = activity.ManagedQuery (contactsUri, contactProjection, null, null, null)
+            let items = getContactsInfo cursor
+            items
+    
+        contacts
---
+++ Contacts2Go/MainActivity.fs
@@ -0,0 +1,45 @@
+﻿namespace Contacts2Go
+
+open System
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Android.Graphics
+open Android.Text.Method
+open Contacts
+
+type Resources = Contacts2Go.Resource
+
+[<Activity (Label = "Contacts2Go", MainLauncher = true, Icon = "@mipmap/icon")>]
+type MainActivity () =
+    inherit Activity ()
+
+    let mutable count:int = 1
+
+    override this.OnCreate (bundle) =
+
+        base.OnCreate (bundle)
+
+        // Set our view from the "main" layout resource
+        this.SetContentView (Resources.Layout.Main)
+
+        // Get our button from the layout resource, and attach an event to it
+        let button = this.FindViewById<Button>(Resources.Id.myButton)
+        let trace = this.FindViewById<TextView>(Resources.Id.trace)
+        trace.set_MovementMethod(new ScrollingMovementMethod());
+
+        button.Click.Add (fun args -> 
+            button.Text <- sprintf "%d clicks!" count
+            count <- count + 1
+
+            let traceMsg = ""
+            // let contactDisplayList = Array.collect (fun (id,name) -> [|sprintf "%s = %s\n" id name|]) (GetContactCursor this)
+            let contactDisplayList = Array.fold (fun msg (id,name) -> msg + (sprintf "%s = %s\n" id name)) "" (GetContactCursor this)
+            trace.SetText(contactDisplayList, TextView.BufferType.Normal)
+            ()
+        )
+
+                
